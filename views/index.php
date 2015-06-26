822454d961e51aa8ea6b85b85e1f2f43d9cbe89d
Elice <valentin.zander@gmail.com>
2015-06-26 21:57:26 +0100

    index
    contenue home page

--- views/index.php
+++ views/index.php
@@ -1,6 +1,29 @@
-<!--meta title=""-->
+<!--meta title="home" css="" js=""-->
 <div id="content">
-	<p>
-		test
-	</p>
+	<div id="login">
+		<form>
+			<input type="text" name="identifiant" hint="nom d'utilisateur">
+			<input type="password" name="password" hint="*********">
+			<input type="submit" name="login" value="login">
+		</form>
+		<a href=""><input type="button" value="register"></a>
+	</div>
+	<div id="search">
+		<form>
+			<input type="text" name="search" hint="recherche...">
+			<input type="submit" name="validateSearch" value="search">
+		</form>
+	</div>
+	<div id="body">
+		<div id="teaser">
+			<p>Creation et partage de musique du monde entier <br> #CoMusique</p>
+			<p>Decouvrez les<a href=""><input type="button" value="Morceaux du moment"></a></p>
+		</div>
+		<div id="more">
+			<p>Musicien solitaire ?Rejoignez le premier groupe de musique communautaire</p>
+			<p>Publié votre creation et laissez les autres ajouter leur morceau</p>
+			<p>Decouvrez la creation de musique en toute simplicite</p>
+			<input type="button" value="Essayez !">
+		</div>
+	</div>
 </div>
