f5bc2ae14f8eac4bb12f3aebddd168aa818a79f9
Elice <valentin.zander@gmail.com>
2015-06-26 22:32:29 +0100

    index
    contenue home page.

--- views/index.php
+++ views/index.php
@@ -1,29 +1,33 @@
 <!--meta title="home" css="" js=""-->
 <div id="content">
-	<div id="login">
+	<section id="login">
 		<form>
-			<input type="text" name="identifiant" hint="nom d'utilisateur">
-			<input type="password" name="password" hint="*********">
+			<input type="text" name="identifiant" placeholder="nom d'utilisateur">
+			<input type="password" name="password" placeholder="*********">
 			<input type="submit" name="login" value="login">
 		</form>
-		<a href=""><input type="button" value="register"></a>
-	</div>
-	<div id="search">
+		<a href="">register</a>
+	</section>
+	<section id="search">
 		<form>
-			<input type="text" name="search" hint="recherche...">
+			<input type="text" name="search" placeholder="recherche...">
 			<input type="submit" name="validateSearch" value="search">
 		</form>
-	</div>
-	<div id="body">
-		<div id="teaser">
+	</section>
+	<section id="body">
+		<section id="teaser">
 			<p>Creation et partage de musique du monde entier <br> #CoMusique</p>
-			<p>Decouvrez les<a href=""><input type="button" value="Morceaux du moment"></a></p>
-		</div>
-		<div id="more">
-			<p>Musicien solitaire ?Rejoignez le premier groupe de musique communautaire</p>
-			<p>Publié votre creation et laissez les autres ajouter leur morceau</p>
+			<p>Decouvrez les <a href="">Morceaux du moment</a></p>
+		</section>
+		<section id="more">
+			<p>Musicien solitaire ? Rejoignez le premier groupe de musique communautaire</p>
+			<p>Publiez votre creation et laissez les autres ajouter leur morceau</p>
 			<p>Decouvrez la creation de musique en toute simplicite</p>
-			<input type="button" value="Essayez !">
-		</div>
-	</div>
+			<a href="">Essayez !</a>
+		</section>
+	</section>
+	<section id="footer">
+		<p>Application developpe par la Team 2A au Hackathon musical</p>
+		<a href="">Mention legal</a>
+	</section>
 </div>
