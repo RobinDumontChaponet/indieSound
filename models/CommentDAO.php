414001a111f27b91bbec285cfab23329bd5bb746
MatthiasCruciani <matthiascruciani@hotmail.fr>
2015-06-27 00:19:40 +0200

    changes dao comment+user
    by He@vy_X

--- models/CommentDAO.php
+++ models/CommentDAO.php
@@ -5,78 +5,88 @@ include_once MODELS_INC.'Comment.class.php';
 
 class CommentDAO {
 
-	public static function create ($comment) {
-		try {
-			$author = $_SESSION['login']->getLogin();
-			$connect=SPDO::getInstance();
-			$statement = $connect->prepare('INSERT INTO comment(author, value, time) VALUES (?, ?, NOW())');
-			$statement->bindParam(1, $author);
-			$comment = $comment->getComment();
-			$statement->bindParam(2, $comment);
-			$statement->execute();
+	public static function create (&$comment) {
+		if (get_class( $obj ) == "Comment") {
+			try {
+				$author = $_SESSION['login']->getLogin();
+				$connect=SPDO::getInstance();
+				$statement = $connect->prepare('INSERT INTO comment(author, value, time) VALUES (?, ?, NOW())');
+				$statement->bindParam(1, $author);
+				$comment = $comment->getComment();
+				$statement->bindParam(2, $comment);
+				$statement->execute();
 
-			return $connect->lastInsertId();
-		} catch (PDOException $e) {
-			die('Error create comment!: ' . $e->getMessage() . '<br/>');
+				return $connect->lastInsertId();
+			} catch (PDOException $e) {
+				die('Error create comment!: ' . $e->getMessage() . '<br/>');
+			}
 		}
 	}
 
 	public static function update ($comment) {
-		try {
-			$connect=SPDO::getInstance();
-			$statement = $connect->prepare('UPDATE comment SET value=? WHERE id=?');
-			$comment = $comment->getcomment();
-			$statement->bindParam(1, $comment);
-			$statement->execute();
+		if (get_class( $obj ) == "Comment") {
+			try {
+				$connect=SPDO::getInstance();
+				$statement = $connect->prepare('UPDATE comment SET value=? WHERE id=?');
+				$comment = $comment->getcomment();
+				$statement->bindParam(1, $comment);
+				$statement->execute();
 
-			return $connect->lastInsertId();
-		} catch (PDOException $e) {
-			die('Error update user!: ' . $e->getMessage() . '<br/>');
+				return $connect->lastInsertId();
+			} catch (PDOException $e) {
+				die('Error update user!: ' . $e->getMessage() . '<br/>');
+			}
 		}
 	}
 
 	public static function delete ($comment) {
-		try {
-			$connect=SPDO::getInstance();
-			$statement = $connect->prepare('DELETE FROM comment WHERE id=?');
-			$id = $comment->getId();
-			$statement->bindParam(1, $id);
-			$statement->execute();
-		} catch (PDOException $e) {
-			die('Error delete user!: ' . $e->getMessage() . '<br/>');
+		if (get_class( $obj ) == "Comment") {
+			try {
+				$connect=SPDO::getInstance();
+				$statement = $connect->prepare('DELETE FROM comment WHERE id=?');
+				$id = $comment->getId();
+				$statement->bindParam(1, $id);
+				$statement->execute();
+			} catch (PDOException $e) {
+				die('Error delete user!: ' . $e->getMessage() . '<br/>');
+			}
 		}
 	}
 
 	public static function getAll () {
-		$comment = array();
-		try {
-			$connect=SPDO::getInstance();
-			$statement = $connect->prepare('SELECT * FROM comment');
+		if (get_class( $obj ) == "Comment") {
+			$comment = array();
+			try {
+				$connect=SPDO::getInstance();
+				$statement = $connect->prepare('SELECT * FROM comment');
 
-			$statement->execute();
+				$statement->execute();
 
-			while ($rs = $statement->fetch(PDO::FETCH_OBJ))
-				$comment[]=new Comment($rs->id, $rs->author, $rs->comment, $rs->time);
-		} catch (PDOException $e) {
-			die('Error!: ' . $e->getMessage() . '<br/>');
+				while ($rs = $statement->fetch(PDO::FETCH_OBJ))
+					$comment[]=new Comment($rs->id, $rs->author, $rs->comment, $rs->time);
+			} catch (PDOException $e) {
+				die('Error!: ' . $e->getMessage() . '<br/>');
+			}
+			return $comment;
 		}
-		return $comment;
 	}
 
 	public static function getById ($id) {
-		$comment = null;
-		try {
-			$connect=SPDO::getInstance();
-			$statement = $connect->prepare('SELECT * FROM comment where id=?');
-			$statement->bindParam(1, $id);
-			$statement->execute();
+		if (get_class( $obj ) == "Comment") {
+			$comment = null;
+			try {
+				$connect=SPDO::getInstance();
+				$statement = $connect->prepare('SELECT * FROM comment where id=?');
+				$statement->bindParam(1, $id);
+				$statement->execute();
 
-			if($rs = $statement->fetch(PDO::FETCH_OBJ))
-				$comment=new Comment($rs->id, $rs->author, $rs->comment, $rs->time);
-		} catch (PDOException $e) {
-			die('Error!: ' . $e->getMessage() . '<br/>');
+				if($rs = $statement->fetch(PDO::FETCH_OBJ))
+					$comment=new Comment($rs->id, $rs->author, $rs->comment, $rs->time);
+			} catch (PDOException $e) {
+				die('Error!: ' . $e->getMessage() . '<br/>');
+			}
+			return $comment;
 		}
-		return $comment;
 	}	
 }
 ?>
--- models/UserDAO.php
+++ models/UserDAO.php
@@ -5,125 +5,139 @@ include_once MODELS_INC.'User.class.php';
 
 class UserDAO {
 
-	public static function create ($user) {
-		try {
-			$connect=SPDO::getInstance();
-			$statement = $connect->prepare('INSERT INTO user(login, password, mail, lastName, firstName) VALUES (?, ?, ?, ?, ?)');
-			$log = $user->getLogin();
+	public static function create (&$user) {
+		if (get_class( $obj ) == "User") {
+			try {
+				$connect=SPDO::getInstance();
+				$statement = $connect->prepare('INSERT INTO user(login, password, mail, lastName, firstName) VALUES (?, ?, ?, ?, ?)');
+				$log = $user->getLogin();
 
-			if (!isExistLogin($log)){
-			$statement->bindParam(1, $log);
-			$pwd = $user->getPassword();
-			$statement->bindParam(2, $pwd);
-			$mail = $user->getMail();
-			$statement->bindParam(3, $mail);
-			$lastName = $user->getLastName();
-			$statement->bindParam(4, $lastName);
-			$firstName = $user->getFirstName();
-			$statement->bindParam(5, $firstName);
-			$statement->execute();
-			} else {
-				return false;
+				if (!isExistLogin($log)){
+				$statement->bindParam(1, $log);
+				$pwd = $user->getPassword();
+				$statement->bindParam(2, $pwd);
+				$mail = $user->getMail();
+				$statement->bindParam(3, $mail);
+				$lastName = $user->getLastName();
+				$statement->bindParam(4, $lastName);
+				$firstName = $user->getFirstName();
+				$statement->bindParam(5, $firstName);
+				$statement->execute();
+				} else {
+					return false;
+				}
+				return $connect->lastInsertId();
+			} catch (PDOException $e) {
+				die('Error create user!: ' . $e->getMessage() . '<br/>');
 			}
-			return $connect->lastInsertId();
-		} catch (PDOException $e) {
-			die('Error create user!: ' . $e->getMessage() . '<br/>');
 		}
 	}
 
 	public static function update ($user) {
-		try {
-			$connect=SPDO::getInstance();
-			$statement = $connect->prepare('UPDATE user SET  login=?, password=?, mail=?, lastName=?, firstName=? WHERE id=?');
-			$statement->bindParam(1, $user->getAuth()->getId());
-			$statement->bindParam(2, $user->getLogin());
-			$statement->bindParam(3, $user->getPwd());
-			$statement->bindParam(4, $user->getId());
-			$statement->execute();
+		if (get_class( $obj ) == "User") {
+			try {
+				$connect=SPDO::getInstance();
+				$statement = $connect->prepare('UPDATE user SET  login=?, password=?, mail=?, lastName=?, firstName=? WHERE id=?');
+				$statement->bindParam(1, $user->getAuth()->getId());
+				$statement->bindParam(2, $user->getLogin());
+				$statement->bindParam(3, $user->getPwd());
+				$statement->bindParam(4, $user->getId());
+				$statement->execute();
 
-			return $connect->lastInsertId();
-		} catch (PDOException $e) {
-			die('Error update user!: ' . $e->getMessage() . '<br/>');
+				return $connect->lastInsertId();
+			} catch (PDOException $e) {
+				die('Error update user!: ' . $e->getMessage() . '<br/>');
+			}
 		}
 	}
 
 	public static function delete ($user) {
-		try {
-			$connect=SPDO::getInstance();
-			$statement = $connect->prepare('DELETE FROM user WHERE id=?');
-			$statement->bindParam(1, $user->getId());
-			$statement->execute();
-		} catch (PDOException $e) {
-			die('Error delete user!: ' . $e->getMessage() . '<br/>');
+		if (get_class( $obj ) == "User") {
+			try {
+				$connect=SPDO::getInstance();
+				$statement = $connect->prepare('DELETE FROM user WHERE id=?');
+				$statement->bindParam(1, $user->getId());
+				$statement->execute();
+			} catch (PDOException $e) {
+				die('Error delete user!: ' . $e->getMessage() . '<br/>');
+			}
 		}
 	}
 
 	public static function getAll () {
-		$user = array();
-		try {
-			$connect=SPDO::getInstance();
-			$statement = $connect->prepare('SELECT * FROM user');
+		if (get_class( $obj ) == "User") {
+			$user = array();
+			try {
+				$connect=SPDO::getInstance();
+				$statement = $connect->prepare('SELECT * FROM user');
 
-			$statement->execute();
+				$statement->execute();
 
-			while ($rs = $statement->fetch(PDO::FETCH_OBJ))
-				$user[]=new User($rs->id, $rs->login, $rs->password, $rs->mail, $rs->lastName, $rs->firstName );
-		} catch (PDOException $e) {
-			die('Error! getAll:' . $e->getMessage() . '<br/>');
+				while ($rs = $statement->fetch(PDO::FETCH_OBJ))
+					$user[]=new User($rs->id, $rs->login, $rs->password, $rs->mail, $rs->lastName, $rs->firstName );
+			} catch (PDOException $e) {
+				die('Error! getAll:' . $e->getMessage() . '<br/>');
+			}
+			return $user;
 		}
-		return $user;
 	}
 
 	public static function getById ($id) {
-		$memrbe = null;
-		try {
-			$connect=SPDO::getInstance();
-			$statement = $connect->prepare('SELECT * FROM user where id=?');
-			$statement->bindParam(1, $id);
-			$statement->execute();
+		if (get_class( $obj ) == "User") {
+			$user = null;
+			try {
+				$connect=SPDO::getInstance();
+				$statement = $connect->prepare('SELECT * FROM user where id=?');
+				$statement->bindParam(1, $id);
+				$statement->execute();
 
-			if($rs = $statement->fetch(PDO::FETCH_OBJ))
-				$user=new User($rs->id, $rs->login, $rs->password, $rs->mail, $rs->lastName, $rs->firstName );
-		} catch (PDOException $e) {
-			die('Error! getById:' . $e->getMessage() . '<br/>');
+				if($rs = $statement->fetch(PDO::FETCH_OBJ))
+					$user=new User($rs->id, $rs->login, $rs->password, $rs->mail, $rs->lastName, $rs->firstName );
+			} catch (PDOException $e) {
+				die('Error! getById:' . $e->getMessage() . '<br/>');
+			}
+			return $user;
 		}
-		return $user;
 	}
 
 	public static function getByLogin ($login) {
-		$user = null;
+		if (get_class( $obj ) == "User") {
+			$user = null;
 
-		try {
-			$connect=SPDO::getInstance();
-			$statement = $connect->prepare('SELECT * FROM user where login=?');
-			$statement->bindParam(1, $login);
-			$statement->execute();
+			try {
+				$connect=SPDO::getInstance();
+				$statement = $connect->prepare('SELECT * FROM user where login=?');
+				$statement->bindParam(1, $login);
+				$statement->execute();
 
-			if($rs = $statement->fetch(PDO::FETCH_OBJ))
-				$user=new User($rs->id, $rs->login, $rs->password, $rs->mail, $rs->lastName, $rs->firstName );
-			} catch (PDOException $e) {
-			die('Error! getByLogin:' . $e->getMessage() . '<br/>');
+				if($rs = $statement->fetch(PDO::FETCH_OBJ))
+					$user=new User($rs->id, $rs->login, $rs->password, $rs->mail, $rs->lastName, $rs->firstName );
+				} catch (PDOException $e) {
+				die('Error! getByLogin:' . $e->getMessage() . '<br/>');
+			}
+			return $user;
 		}
-		return $user;
 	}
 
 	public static function isExistLogin ($login) {
-		$user = null;
+			if (get_class( $obj ) == "User") {
+			$user = null;
 
-		try {
-			$connect=SPDO::getInstance();
-			$statement = $connect->prepare('SELECT * FROM user where login=?');
-			$statement->bindParam(1, $login);
-			$statement->execute();
+			try {
+				$connect=SPDO::getInstance();
+				$statement = $connect->prepare('SELECT * FROM user where login=?');
+				$statement->bindParam(1, $login);
+				$statement->execute();
 
-			if($rs = $statement->fetch(PDO::FETCH_OBJ))
-				if(empty($rs)) {
-					return false;
-				} else {
-					return true;
-				}
-			} catch (PDOException $e) {
-			die('Error! isExistLogin:' . $e->getMessage() . '<br/>');
+				if($rs = $statement->fetch(PDO::FETCH_OBJ))
+					if(empty($rs)) {
+						return false;
+					} else {
+						return true;
+					}
+				} catch (PDOException $e) {
+				die('Error! isExistLogin:' . $e->getMessage() . '<br/>');
+			}
 		}
 	}
 }
