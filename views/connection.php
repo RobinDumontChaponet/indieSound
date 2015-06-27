e405af02e9f2c2d01341e2739c4deb7e25f32042
Benshu <schuh.benjamin.wil@gmail.com>
2015-06-27 13:59:35 +0200

    css connection start
    Un debut de css, modifer le type d'affichage !

--- views/connection.php
+++ views/connection.php
@@ -1,12 +1,14 @@
 <!--meta title="Connexion" css="style/connection.css"-->
-<form method="post" action="">
-	<legend>Se Connecter</legend>
-	<label>Nom d'utilisateur</label>
-	<input type="text" name="login" placeholder="Nom d'utilisateur"/>
-	<label>Mot de passe</label>
-	<input type="password" name="password" placeholder="Mot de passe"/>
-	<a href="forgot" rel="forgot_password">mot de passe oublié ?</a>
-	<button type="submit" name="submit">Connexion</button>
-	<a href="register">Vous n'avez pas de compte ? S'inscrire</a>
-	<?php if ($badinput===true) echo'<p class="badpass">Identifiant ou mot-de-passe incorrects !</p>';?>
-</form>
+<div id="content">
+	<form method="post" action="">
+
+		<label>Nom d'utilisateur</label>
+		<input type="text" name="login" placeholder="Nom d'utilisateur"/>
+		<label>Mot de passe</label>
+		<input type="password" name="password" placeholder="Mot de passe"/>
+		<a href="forgot" rel="forgot_password">Mot de passe oublié ?</a>
+		<button type="submit" name="submit">Connexion</button>
+		<a href="register">Vous n'avez pas de compte ? S'inscrire</a>
+		<?php if ($badinput===true) echo'<p class="badpass">Identifiant ou mot-de-passe incorrects !</p>';?>
+	</form>
+</div>
