4a97ef142eca52eccfec4dd1a8a2cecadb2c007c
Benshu <schuh.benjamin.wil@gmail.com>
2015-06-27 18:37:10 +0200

    Ajout partie Contact
    Reste a regler le controlleur pour l'envoi de mail

---
+++ views/contact.php
@@ -0,0 +1,20 @@
+ <!--meta title="contact" css="style/contact.css"-->
+<div id="content">
+  
+   <h2>N'hesitez pas a prendre contact ! </h2> 
+
+        <h4>Contact details</h4>
+        <p> <i class="fa fa-map-marker fa-lg"></i> TCRM BLIDA Metz 57000</p>
+        <p> <i class="fa fa-mobile fa-lg"></i> +33 06 49 62 31 55</p>
+        <p> <i class="fa fa-envelope-o "></i> <a href="mailto:indieSound@gmail.com">indieSound@gmail.com</a></p>
+
+
+        <h4>Laissez un message!</h4>
+        <form method="post" action="contact.php" name="cform" id="cform">
+          <input name="name" id="name" type="text" class="col-xs-12 col-sm-6 col-md-6 col-lg-6" placeholder="Votre nom..." >
+          <input name="email" id="email" type="email" class=" col-xs-12 col-sm-6 col-md-6 col-lg-6 noMarr" placeholder="Votre email..." >
+          <textarea name="comments" id="comments" cols="" rows="" class="col-xs-12 col-sm-12 col-md-12 col-lg-12" placeholder="Votre message..."></textarea>
+          <input type="submit" id="submit" name="send" class="submitBnt" value="Send message">
+          <div id="simple-msg"></div>
+        </form>
+</div>
