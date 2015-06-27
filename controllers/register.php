5516d7b91db3fdf54f9d1676e86453630aa3da52
Victor Jozwicki <tigre.demon@hotmail.fr>
2015-06-27 11:32:05 +0200

    Register

--- controllers/register.php
+++ controllers/register.php
@@ -1,24 +1,44 @@
 <?php
-  
-if(ISSET($_POST['submit'])) {
+
+if($_POST) {
 	include ('validate.transit.inc.php');
-	include('conf.inc.php');
 	include(MODELS_INC.'UserDAO.php');
 
-	$password = $_POST['password'];
-	$verifPassword = $_POST['verifPassword'];
-	$nom = $_POST['nom'];
-	$prenom = $_POST['prenom'];
-	$mail = $_POST['mail'];
-	$sexe = $_POST['sexe'];
-	$login = $_POST['user'];
+    if( $_POST['login'] != NULL ) {
+        $login = $_POST['login'];
+        var_dump($login);
+    } else {
+        $errorLogin = true;
+    }
+
+    if( $_POST['password'] != NULL ) {
+        $password = $_POST['password'];
+        var_dump($password);
+    } else {
+        $errorPassword = true;
+    }
+
+    if( $_POST['mail'] != NULL) {
+        $mail = $_POST['mail'];
+        var_dump($mail);
+    } else {
+        $errorMail = true;
+    }
+
+    if( $_POST['mailConfirmation'] != NULL ) {
+        $mailConfirmation = $_POST['mailConfirmation'];
+        var_dump($mailConfirmation);
+    } else {
+        $errorMailConfirmation = true;
+    }
+
 	//$password = hash("sha256", $password);
 
 
-	if(valideNom($nom) && validePrenom($prenom) && valideMail($mail) && valideUser($login) && !empty($sexe) && validePwd($password) && $verifPassword==$password) {
-		$user=MembreDAO::create (new Membre('', $login, $password, $mail, $sexe, $nom, $prenom, ''));
-		header('Location: index.php');
-	}
+	/*if(valideNom($nom) && validePrenom($prenom) && valideMail($mail) && valideUser($login) && !empty($sexe) && validePwd($password) && $verifPassword==$password) {
+		//$user=MembreDAO::create (new Membre('', $login, $password, $mail, $sexe, $nom, $prenom, ''));
+		//header('Location: index.php');
+	}*/
 }
 
 include(VIEWS_INC.'register.php');
