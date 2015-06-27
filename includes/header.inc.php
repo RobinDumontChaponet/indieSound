2b76c3bba9b587b1d1acbbe2ab0535e4599a3271
RobinDumontChaponet <src@dumontchapo.net>
2015-06-27 18:36:21 +0200

    style

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -4,24 +4,23 @@
 		<ul>
 			<li<?= ($_GET['requ']=='all')?' class="active" ':'' ?>><a href="all"><span>Tous les morceaux</span></a></li>
 			<li<?= ($_GET['requ']=='new')?' class="active" ':'' ?>><a href="new"><span>Nouveau projet</span></a></li>
-			<li><a href="#" id="searchLI"><span>Recherche</span></a></li>
+			<li id="searchLi"><a href="#"><span>Recherche</span></a>
+				<form name="search">
+					<input type="text" name="searchValue" placeholder="Recherche..."/>
+					<input type="submit" value="Rechercher"/>
+        		</form>
+        	</li>
 			<?php if(empty($_SESSION['stUser'])) { ?>
 			<li <?= ($_GET['requ']=='connection')?' class="active" ':'' ?>><a href="connection"><span>Se connecter</span></a></li>
 			<?php } else { ?>
-			<li class "sub"><a href="#"><?= $_SESSION['stUser']->getLogin() ?></a>
+			<li<?= ($_GET['requ']=='profile')?' class="active" ':'' ?> id="userLi" class="sub"><a href="profile"><?= $_SESSION['stUser']->getLogin() ?></a>
 				<ul>
 					<li><a href="#">Notifications</a></li>
-					<li><a href="#">Réglages</a></li>
+					<li><a href="parameter">Réglages</a></li>
 					<li><a href="logout">Se déconnecter</a></li>
 				</ul>
 			</li>
 			<?php } ?>
 		</ul>
 	</nav>
-	<aside>
-        <form name="search">
-            <input type="text" name="searchValue" placeholder="Recherche..."/>
-            <input type="submit" value="Rechercher"/>
-        </form>
-    </aside>
 </header>
