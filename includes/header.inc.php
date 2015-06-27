e662d2b333d051a3a853a5e19673fd80b9339100
RobinDumontChaponet <src@dumontchapo.net>
2015-06-27 14:20:51 +0200

    style menu

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -2,15 +2,14 @@
 	<h1><a href="<?= SELF ?>">Sound Tree</a></h1>
 	<nav>
 		<ul>
-			
 			<li><a href="#">Tout les morceaux</a></li>
 			<li><a href="#">Nouveau projet</a></li>
 			<li><a href="#">Contact</a></li>
 			<li class "sub"><a href="#"> Nom du Compte<!--<?= ($_SESSION['stUser']) ?> --></a>
-				<div>
-					<a href="#"> Notifications </a>
-					<a href="#"> Réglages </a>
-				</div>
+				<ul>
+					<li><a href="#">Notifications</a></li>
+					<li><a href="#"> Réglages </a></li>
+				</ul>
 			</li>
 			<li><a href="connection">Se connecter</a></li>
 		</ul>
