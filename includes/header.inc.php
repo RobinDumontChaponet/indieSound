83ae569b24e6035f3bd1819875165657fd497dff
RobinDumontChaponet <src@dumontchapo.net>
2015-06-27 17:21:06 +0200

    icons and things...

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -1,25 +1,23 @@
 <header>
-	<h1><a href="<?= SELF ?>">Sound Tree</a></h1>
+	<h1><a href="<?= SELF ?>"><span>IndieSound</span></a></h1>
 	<nav>
 		<ul>
-			<li><a href="#">Tout les morceaux</a></li>
-			<li><a href="#">Nouveau projet</a></li>
-			<li><a href="#">Contact</a></li>
+			<li<?= ($_GET['requ']=='all')?' class="active" ':'' ?>><a href="all"><span>Tous les morceaux</span></a></li>
+			<li<?= ($_GET['requ']=='new')?' class="active" ':'' ?>><a href="new"><span>Nouveau projet</span></a></li>
+			<li><a href="#" id="searchLI"><span>Recherche</span></a></li>
 			<?php if(empty($_SESSION['stUser'])) { ?>
-			<li><a href="connection">Se connecter</a></li>
+			<li <?= ($_GET['requ']=='connection')?' class="active" ':'' ?>><a href="connection"><span>Se connecter</span></a></li>
 			<?php } else { ?>
 			<li class "sub"><a href="#"><?= $_SESSION['stUser']->getLogin() ?></a>
 				<ul>
 					<li><a href="#">Notifications</a></li>
-					<li><a href="#"> Réglages </a></li>
+					<li><a href="#">Réglages</a></li>
 					<li><a href="logout">Se déconnecter</a></li>
 				</ul>
 			</li>
 			<?php } ?>
 		</ul>
 	</nav>
-	
-	
 	<aside>
         <form name="search">
             <input type="text" name="searchValue" placeholder="Recherche..."/>
