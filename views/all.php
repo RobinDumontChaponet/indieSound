aa892904466af1efad6f09b97e872065a5f3f60c
Elice <valentin.zander@gmail.com>
2015-06-28 12:52:41 +0100

    facebook
    shaaare (ou presque)

--- views/all.php
+++ views/all.php
@@ -1,10 +1,12 @@
-<!--meta title="Tous les morceaux"-->
+<!--meta title="Tous les morceaux" css="style/all.css"-->
 <div id="content">
 	<ul>
 	<?php
 	foreach($projects as $project) {
-		echo '<li><a class="aProject" href="project/'.$project->getId().'">'.$project->getName().'</a>'. ' ' .'<a class="aUser" href="#">'.$project->getOwner()->getLogin().'</a> 57 version disponible </li>';
-	}
+		echo '<li ><a class="aProject" href="project/'.$project->getId().'">'.$project->getName().'</a>'. '
+		' .'<a class="aUser" href="#">'.$project->getOwner()->getLogin().'</a> 57 version disponible
+		<button id=""facebook"></button></li>';
+		}
 	?>
 	</ul>
 </div>
