57228b72772efccd8a67a3de9263465f3d00713d
RobinDumontChaponet <src@dumontchapo.net>
2015-06-28 14:03:54 +0200

    argh

--- views/project.php
+++ views/project.php
@@ -21,7 +21,7 @@
 		<nav id="timeline"></nav>
 		<dl>
 			<dt>Piste 1<button class="deletePiste"><span>Supprimer</span></button></dt>
-			<dd><div data-time="0" data-length="1">son1</div><div data-time="2" data-length="2">son2</div></dd>
+			<dd><div data-time="0" data-length="1">son1</div><div data-time="2" data-length="2">son2</div><div data-time="2" data-length="2">son2</div><div data-time="2" data-length="2">son2</div><div data-time="2" data-length="2">son2</div><div data-time="2" data-length="2">son2</div><div data-time="2" data-length="2">son2</div><div data-time="2" data-length="2">son2</div><div data-time="2" data-length="2">son2</div><div data-time="2" data-length="2">son2</div><div data-time="2" data-length="2">son2</div><div data-time="2" data-length="2">son2</div></dd>
 			<dt>Piste 2<button class="deletePiste"><span>Supprimer</span></button></dt>
 			<dd><div data-time="1" data-length="1">son1</div><div data-time="4" data-length="3">son2</div></dd>
 			<dt><button id="addTrack">ajouter une piste</button></dt>
