69157af5fcfc362f726b8081c24134950cf8f6bd
Benshu <schuh.benjamin.wil@gmail.com>
2015-06-28 13:54:36 +0200

    Ajout Top 3 musique

--- views/index.php
+++ views/index.php
@@ -1,20 +1,107 @@
-<!--meta title="index"-->
+<!--meta title="index" css="style/project.css" js="script/blip.min.js" js="script/project.js"-->
 <div id="content">
 
-	<section id="more">
+	<section id="teaser">
 		<p>Musicien solitaire ? Rejoignez le premier groupe de musique communautaire</p>
 		<p>Publiez vos créations et laissez les autres y contribuer</p>
 		<p>Découvrez la création de musique en toute simplicité</p>
 		<a href="create">Essayez !</a>
 	</section>
-<<<<<<< HEAD
 
+	<section>
+		<h2> Morceaux du moment</h2>
+			<ul>
 
-	<section id="teaser">
+            <li class="soundList__item">
+
+				<div class= "titleSong"> 
+					<p class="title"><button id="music">Instrumental Intro Basic Rock </button></p>
+					<p class="author"><button id="user">Mickaël Berko</button></p>
+				</div>
+
+				<div class= "ImgSong">
+					<img src=""/>
+
+				</div>
+
+				<div class="ControlSong">
+					<button id="rewind"></button>
+					<button id="play"></button>
+					<button id="volume-up"></button>
+				</div>
+
+				<div class= "SocialSong">
+					<button id="heart-o"></button>
+					<button id="facebook"></button>
+					<button id="twitter"></button>
+					<button id="google-plus"></button>
+				</div>
+
+            </li>
+
+            <li class="soundList__item">
+				<div class= "titleSong"> 
+					<p class="title"><button id="music">Instrumental Intro Basic Rock </button></p>
+					<p class="author"><button id="user">Mickaël Berko</button></p>
+				</div>
+
+				<div class= "ImgSong">
+					<img src=""/>
+
+				</div>
+
+				<div class="ControlSong">
+					<button id="rewind"></button>
+					<button id="play"></button>
+					<button id="volume-up"></button>
+				</div>
+
+				<div class= "SocialSong">
+					<button id="heart"></button>
+					<button id="facebook"></button>
+					<button id="twitter"></button>
+					<button id="google-plus"></button>
+				</div>
+
+            </li>
+
+            <li class="soundList__item">
+				<div class= "titleSong"> 
+					<p class="title"><button id="music">Instrumental Intro Basic Rock </button></p>
+					<p class="author"><button id="user">Mickaël Berko</button></p>
+				</div>
+
+				<div class= "ImgSong">
+					<img src=""/>
+
+				</div>
+
+				<div class="ControlSong">
+					<button id="rewind"></button>
+					<button id="play"></button>
+					<button id="volume-up"></button>
+				</div>
+
+				<div class= "SocialSong">
+					<button id="heart"></button>
+					<button id="facebook"></button>
+					<button id="twitter"></button>
+					<button id="google-plus"></button>
+				</div>
+
+            </li>
+
+            </ul>
+	</section>
+
+	<section id="more">
 		<p>Création et partage de musique du monde entier <br> #CoMusique</p>
 		<p>Découvrez les <a href="trend">Morceaux du moment</a></p>
 	</section>
 </div>
-=======
-</div>
->>>>>>> origin/master
+		
+
+
+
+
+   
