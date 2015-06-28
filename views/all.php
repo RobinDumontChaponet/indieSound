ac5a4f1b78ccebdb4ec1f87724b64edbc64ad82a
Elice <valentin.zander@gmail.com>
2015-06-28 13:12:25 +0100

    social 4 all
    social

--- views/all.php
+++ views/all.php
@@ -5,7 +5,12 @@
 	foreach($projects as $project) {
 		echo '<li ><a class="aProject" href="project/'.$project->getId().'">'.$project->getName().'</a>'. '
 		' .'<a class="aUser" href="#">'.$project->getOwner()->getLogin().'</a> 57 version disponible
-		<button id=""facebook"></button></li>';
+		<div class= "SocialSong">
+					<button id="heart"></button>
+					<button id="facebook"></button>
+					<button id="twitter"></button>
+					<button id="google-plus"></button>
+				</div></li>';
 		}
 	?>
 	</ul>
