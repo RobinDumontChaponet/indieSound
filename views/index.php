33b1efb817e315395d26f22361d25b03fd348ea9
Benshu <schuh.benjamin.wil@gmail.com>
2015-06-28 10:49:45 +0200

    test .gitignore

--- views/index.php
+++ views/index.php
@@ -1,13 +1,16 @@
 <!--meta title="index"-->
 <div id="content">
-	<section id="teaser">
-		<p>Création et partage de musique du monde entier <br> #CoMusique</p>
-		<p>Découvrez les <a href="trend">Morceaux du moment</a></p>
-	</section>
+
 	<section id="more">
 		<p>Musicien solitaire ? Rejoignez le premier groupe de musique communautaire</p>
 		<p>Publiez vos créations et laissez les autres y contribuer</p>
 		<p>Découvrez la creation de musique en toute simplicité</p>
 		<a href="create">Essayez !</a>
+	</section>
+
+
+	<section id="teaser">
+		<p>Création et partage de musique du monde entier <br> #CoMusique</p>
+		<p>Découvrez les <a href="trend">Morceaux du moment</a></p>
 	</section>
 </div>
