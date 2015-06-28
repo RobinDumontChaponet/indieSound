8b9f035c85b3b71925e8c4e14b91f806e47f3fb9
RobinDumontChaponet <src@dumontchapo.net>
2015-06-28 09:04:59 +0200

    DAO & gitIgnore

--- models/NoteDAO.class.php
+++ models/NoteDAO.class.php
@@ -71,23 +71,43 @@ class NoteDAO {
 	}
 
 	public static function getById ($id) {
-			$note = null;
-			try {
-				$connect=SPDO::getInstance();
-				$statement = $connect->prepare('SELECT * FROM note where idNote=?');
-				$statement->bindParam(1, $id);
-				$statement->execute();
+		$note = null;
+		try {
+			$connect=SPDO::getInstance();
+			$statement = $connect->prepare('SELECT * FROM note where idNote=?');
+			$statement->bindParam(1, $id);
+			$statement->execute();
 
-				if($rs = $statement->fetch(PDO::FETCH_OBJ)) {
-					$sound = SoundDAO::getById($rs->sound);
+			if($rs = $statement->fetch(PDO::FETCH_OBJ)) {
+				$sound = SoundDAO::getById($rs->sound);
 
-					$note=new Note($rs->idNote, $sound, $rs->startTime);
-				}
-			} catch (PDOException $e) {
-				die('Error! getById:' . $e->getMessage() . '<br/>');
+				$note=new Note($rs->idNote, $sound, $rs->startTime);
 			}
-			return $note;
+		} catch (PDOException $e) {
+			die('Error! getById:' . $e->getMessage() . '<br/>');
+		}
+		return $note;
 	}
+
+	public static function getBySoundIdAndProjectId($soundId, $projectId) {
+		$notes = array();
+		try {
+			$connect=SPDO::getInstance();
+			$statement = $connect->prepare('SELECT * FROM note where sound=? AND ...');
+			$statement->bindParam(1, $soundId);
+			$statement->execute();
+
+			while ($rs = $statement->fetch(PDO::FETCH_OBJ)) {
+				$sound = SoundDAO::getById($rs->sound);
+
+				$notes[]=new Note($rs->idNote, $sound, $rs->startTime);
+			}
+		} catch (PDOException $e) {
+			die('Error! getAll:' . $e->getMessage() . '<br/>');
+		}
+		return $notes;
+	}
+
 }
 
 ?>
